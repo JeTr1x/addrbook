cf59770a4765016a5f830c52e23daa976ef62d2e
isafin509 <70804056+isafin509@users.noreply.github.com>
2022-08-04 16:01:57 +0300

    Update minima-fixid.sh

--- minima/minima-fixid.sh
+++ minima/minima-fixid.sh
@@ -4,13 +4,13 @@ nid="nodeid"
 
 while :
 do
-    sleep 10
+    sleep 30
     tid=$(curl 127.0.0.1:9002/incentivecash | jq -r '.response.uid')
     echo "Current id: $tid"
-    echo "Your id $nid"
+    echo "Your id: $nid"
     if [[ "$nid" != "$tid" ]]; then
         echo "Missmatch, Fixing..."
         curl 127.0.0.1:9002/incentivecash%20uid:$nid
-        echo "Fixed!"
+        echo "  Fixed!"
     fi
 done
